c0d73fc1a5e40b53a33a33d37ffe84d905dc9994
Parth <108parthnikam@gmail.com>
2026-05-13 13:40:11 +0530

    fix sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Sidebar from "@/components/Sidebar.tsx";
+import Sidebar from "../components/Sidebar";
 
 export const metadata: Metadata = {
   title: "Parth Nikam",
